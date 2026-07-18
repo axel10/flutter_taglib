c404963a0de06fafda14999ba48638db05ac22d3
axel10 <axel10@163.com>
2026-07-18 09:30:59 +0800

    example添加歌曲毫秒级详细时间

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -541,9 +541,11 @@ class _MetadataEditorScreenState extends State<MetadataEditorScreen> {
 
   String _formatDuration(Duration duration) {
     String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String threeDigits(int n) => n.toString().padLeft(3, '0');
     final minutes = twoDigits(duration.inMinutes.remainder(60));
     final seconds = twoDigits(duration.inSeconds.remainder(60));
-    return '$minutes:$seconds';
+    final milliseconds = threeDigits(duration.inMilliseconds.remainder(1000));
+    return '$minutes:$seconds.$milliseconds';
   }
 
   @override
